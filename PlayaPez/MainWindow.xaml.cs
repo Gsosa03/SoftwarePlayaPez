f71bb6f8a480da8f2c288ef4082be1aea073e5a4
Gabriel <gsosa4812@gmail.com>
2022-06-24 11:13:30 -0500

    Pimercomit

---
+++ PlayaPez/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace PlayaPez
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEstadistica(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page.Estadistica();
+        }
+
+        private void BtnOxigeno(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page.SensorOxigeno();
+        }
+
+        private void Mostrar(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ocultar(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
